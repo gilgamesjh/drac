e5b493813606787f0e54ee8b9f5bd17668577820
Bjørn Cintra <bcintra@gmail.com>
2014-02-14 14:03:27 +0100

    Added Session object

---
+++ com.cra.drac/src/test/groovy/com/cra/drac/api/Session.groovy
@@ -0,0 +1,36 @@
+package com.cra.drac.api
+
+import com.cra.drac.interfaces.ISession
+
+class Session implements ISession {
+	String serverName
+	
+	@Override
+	public void login() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void login(String userName, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getUserName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String serverName() {
+		return serverName
+	}
+
+	@Override
+	public void serverName(String serverName) {
+		this.serverName = serverName
+	}
+
+}
